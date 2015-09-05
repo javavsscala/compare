e2124814af74dc534f9df9a031f84d28fac4bc6a
Umesh K <fulcrum@hotmail.com>
2015-09-05 09:09:43 -0700

    Added new comments to explain FiniteDuration, imports and implicits

--- day-3/src/main/scala/ScalaCallTimeCalculator.scala
+++ day-3/src/main/scala/ScalaCallTimeCalculator.scala
@@ -21,9 +21,27 @@ object ScalaCallTimeCalculator extends App {
     diff
   }
 
+  /**
+   * Examine the longOperation method. In particular the use of a FiniteDuration instance.
+   * It demonstrates a number scala features.
+   * "2 seconds" translates to 2.seconds().
+   *  If the method has no parameters in parameter then the parenthesis can be dropped.
+   *  The dot between an instance and the method name can be dropped as well.
+   *  However there is no seconds() method on the Int class.
+   *  So yet another feature called implicits is used.
+   *  The implicit feature looks for a class in scope that can use "2"(Int) to construct an instance of itself.
+   *  In addition that class must have a method seconds()
+   *  DurationInt is the class that satisfies this constraint.
+   *  The third item to notice in this case is that the import is done inside the method longOperation.
+   *  This feature allows imports to be be done closest to the point of use.
+   *  The scope of the import is restricted to the block in which it is declared and blocks inside it.
+   */
   //Sample Run
   def longOperation(): Unit = {
-    Thread.sleep(1000)
+    import scala.concurrent.duration._
+    val time: FiniteDuration = 2 seconds
+    val ms =  time.toMillis
+    Thread.sleep(time.toMillis)
   }
 
   val timeInMillisecs = timerMS {
